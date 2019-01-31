6f08add5ed8d264188c8a0ac08181b748c3edf9c
CodeColorist <codecolorist@gmail.com>
2019-01-31 14:32:27 +0800

    remove NSLog timestamp

--- schemes.m
+++ schemes.m
@@ -6,8 +6,11 @@
 #import <Foundation/Foundation.h>
 #import <AppKit/AppKit.h>
 
+#include <stdio.h>
+
 extern OSStatus _LSCopySchemesAndHandlerURLs(CFArrayRef *outSchemes, CFArrayRef *outApps);
 extern OSStatus _LSCopyAllApplicationURLs(CFArrayRef *theList);
+
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
@@ -18,15 +21,13 @@ int main(int argc, const char * argv[]) {
         for (CFIndex i = 0, count = CFArrayGetCount(schemes); i < count; i++) {
             CFStringRef scheme = CFArrayGetValueAtIndex(schemes, i);
             CFArrayRef handlers = LSCopyAllHandlersForURLScheme(scheme);
-            NSLog(@"%@:", scheme);
-
+            printf("-+--= %s\n", [(__bridge NSString *)scheme UTF8String]);
             for (CFIndex j = 0, bundle_count = CFArrayGetCount(handlers); j < bundle_count; j++) {
                 CFStringRef handler = CFArrayGetValueAtIndex(handlers, j);
-                NSLog(@"\t%@ (%@)", handler, [workspace absolutePathForAppBundleWithIdentifier:(__bridge NSString *)handler]);
+                NSString *bundle = [workspace absolutePathForAppBundleWithIdentifier:(__bridge NSString *)handler];
+                printf(" |--= %s (%s)\n", [(__bridge NSString *)handler UTF8String], [bundle UTF8String]);
             }
         }
-        NSLog(@"\n");
     }
     return 0;
 }
-
